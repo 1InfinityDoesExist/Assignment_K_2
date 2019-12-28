b6150cd137668347cc263ca51b68b23c317f2dc7
avinash <avinash.patel@kuliza.com>
2019-12-28 11:40:33 +0530

    Spring Dependency In Details

---
+++ src/test/java/com/spring/dependencyInjection/SpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dependencyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
