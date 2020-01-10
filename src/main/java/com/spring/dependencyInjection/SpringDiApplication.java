86709da5c56364413d5124ed43224314c8a7ce4b
avinash <avinash.patel@kuliza.com>
2020-01-10 16:35:03 +0530

    Static Static Method

--- src/main/java/com/spring/dependencyInjection/SpringDiApplication.java
+++ src/main/java/com/spring/dependencyInjection/SpringDiApplication.java
@@ -1,15 +1,28 @@
 package com.spring.dependencyInjection;
 
-import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
+	private static Logger logger = LoggerFactory.getLogger(SpringDiApplication.class);
+
+	static {
+		System.out.println("************************I am inside Demo Class Logger***************");
+	}
+
+	public static void display() {
+		System.out.println("**************I am inside display()**********************************");
+	}
 
 	public static void main(String[] args) {
 		// PropertyConfigurator.configure("log4j.properties");
-		SpringApplication.run(SpringDiApplication.class, args);
-	}
+		ApplicationContext applicationConext = SpringApplication.run(SpringDiApplication.class, args);
+		System.out.println("*************I am inside Main Method**********************************");
+		logger.info("Application Context:- " + applicationConext);
 
+	}
 }
